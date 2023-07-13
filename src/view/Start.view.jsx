525ec5755d8ef79621b921e7cf1571f2aad16927
Micaela <micaelauvilla9@gmail.com>
2023-07-12 22:18:21 -0300

    changes router dom

--- src/view/Start.view.jsx
+++ src/view/Start.view.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import globantImage from "../assets/"";
+import globantImage from "../assets/Globant-Original1.png";
 
 const Start = () => {
-  return <img src={globantImage} alt="Globant Logo"></img>;
+  return <img src={globantImage} alt="Globant Logo" />;
 };
 
 export default Start;
