63618d9886af3f7a6aae6de719e62f4fc3809ba0
Micaela <micaelauvilla9@gmail.com>
2023-07-12 22:17:01 -0300

    add route profile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
+import React from "react";
 import { Route, Routes } from "react-router";
 import Start from "./view/Start.view";
+import Profile from "./components/Profile.jsx";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={Start}></Route>
+      <Route exact path="/" element={<Start />} />
+      <Route path="/perfil" element={<Profile />} />
     </Routes>
   );
 }
