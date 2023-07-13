5596d1a4c6f3cee575eaa0ff7b8b8755beea2d97
Micaela <micaelauvilla9@gmail.com>
2023-07-13 18:07:40 -0300

    profile funcionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import "./App.css";
 import React from "react";
-import { Route, Routes } from "react-router";
 import Start from "./view/Start.view";
-
 import Profile from "./components/Profile.jsx";
-
 import Register from "./view/Register.view";
-
+import { Route, Routes } from "react-router";
 
 function App() {
   return (
     <Routes>
-
- 
-      <Route path="/perfil" element={<Profile />} />
-
       <Route exact path="/" element={<Start />}></Route>
-      <Route path="/register" element={<Register/>}></Route>
-
+      <Route path="/register" element={<Register />}></Route>
+      <Route path="/perfil/:username" element={<Profile />} />
     </Routes>
   );
 }
