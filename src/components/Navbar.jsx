27b728eeb3bca80bf88890644fa3ebacb1a93cda
v4leng <valem.g_13@hotmail.com>
2023-07-17 17:57:16 -0300

    Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import SwipeableTemporaryDrawer from "../commons/ButtonHamburgerMenu";
+
+function ResponsiveAppBar() {
+  return (
+    <AppBar position="static" sx={{ background: "white" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Button
+            color="inherit"
+            sx={{
+              color: "#3AB54A",
+              display: { xs: "none", md: "flex" },
+              mr: 1
+            }}
+          >
+            Reports
+          </Button>
+          <Button
+            color="inherit"
+            sx={{
+              color: "#3AB54A",
+              display: { xs: "none", md: "flex" },
+              mr: 1
+            }}
+          >
+            Profile
+          </Button>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <SwipeableTemporaryDrawer />
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}></Box>
+          <Button color="inherit" sx={{ color: "#3AB54A" }}>
+            Logout
+          </Button>
+          <Box sx={{ flexGrow: 0 }}></Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default ResponsiveAppBar;
---
+++ src/view/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ResponsiveAppBar from "../components/Navbar";
+
+const Home = () => {
+  return (
+   <>
+   <ResponsiveAppBar />
+   </>
+  );
+};
+
+export default Home;
