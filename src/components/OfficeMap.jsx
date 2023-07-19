0a6c1774d7f78c2279955730ee7ba2498931b746
Micaela <micaelauvilla9@gmail.com>
2023-07-19 18:32:29 -0300

    add desktop selections

--- src/components/OfficeMap.jsx
+++ src/components/OfficeMap.jsx
@@ -1,68 +1,177 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ResponsiveAppBar from './Navbar';
+import { Button, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 function OfficeMap() {
+	const [selectedDesk, setSelectedDesk] = useState(null);
+
+	const handleDeskClick = boxNumber => {
+		setSelectedDesk(boxNumber);
+	};
+	const renderBoxes = (start, end) => {
+		const boxes = [];
+		for (let i = start; i <= end; i++) {
+			const isSelected = selectedDesk === i;
+			boxes.push(
+				<div
+					key={i}
+					className={`column ${isSelected ? 'selected' : ''}`}
+					onClick={() => handleDeskClick(i)}
+				>
+					{i}
+				</div>,
+			);
+		}
+		return boxes;
+	};
+
 	return (
 		<>
+			<ResponsiveAppBar />
+
+			{!selectedDesk ? (
+				<Typography sx={{ textAlign: 'center', marginTop: '5px' }} variant='h6'>
+					Please select your desk
+				</Typography>
+			) : (
+				<Typography sx={{ textAlign: 'center', marginTop: '5px' }} variant='h6'>
+					Your desk is :{selectedDesk}
+				</Typography>
+			)}
+
 			<div className='grid-container'>
 				<div className='container'>
-					<div className='top-container'>1</div>
+					<div
+						className={`top-container ${selectedDesk === 1 ? 'selected' : ''}`}
+						onClick={() => handleDeskClick(1)}
+					>
+						1
+					</div>
 					<div className='column-container'>
-						<div className='column'>2</div>
-						<div className='column'>3</div>
-						<div className='column'>4</div>
-						<div className='column'>5</div>
-						<div className='column'>6</div>
-						<div className='column'>7</div>
-						<div className='top-container'>8</div>
+						{renderBoxes(2, 7)}
+						<div
+							className={`top-container ${
+								selectedDesk === 8 ? 'selected' : ''
+							}`}
+							onClick={() => handleDeskClick(8)}
+						>
+							8
+						</div>
 					</div>
 				</div>
 				<div className='container'>
-					<div className='top-container'>9</div>
+					<div
+						className={`top-container ${selectedDesk === 9 ? 'selected' : ''}`}
+						onClick={() => handleDeskClick(9)}
+					>
+						9
+					</div>
 					<div className='column-container'>
-						<div className='column wide'>10</div>
-						<div className='column wide'>11</div>
-						<div className='column wide'>12</div>
-						<div className='column wide'>13</div>
-						<div className='column wide'>14</div>
-						<div className='column wide'>15</div>
-						<div className='top-container'>16</div>
+						{renderBoxes(10, 15)}
+						<div
+							className={`top-container ${
+								selectedDesk === 16 ? 'selected' : ''
+							}`}
+							onClick={() => handleDeskClick(16)}
+						>
+							16
+						</div>
 					</div>
 				</div>
 				<div className='container'>
-					<div className='top-container'>17</div>
+					<div
+						className={`top-container ${selectedDesk === 17 ? 'selected' : ''}`}
+						onClick={() => handleDeskClick(17)}
+					>
+						17
+					</div>
 					<div className='column-container'>
-						<div className='column wide'>18</div>
-						<div className='column wide'>19</div>
-						<div className='column wide'>20</div>
-						<div className='column wide'>21</div>
-						<div className='column wide'>22</div>
-						<div className='column wide'>23</div>
-						<div className='top-container'>24</div>
+						{renderBoxes(18, 23)}
+						<div
+							className={`top-container ${
+								selectedDesk === 24 ? 'selected' : ''
+							}`}
+							onClick={() => handleDeskClick(24)}
+						>
+							24
+						</div>
 					</div>
 				</div>
 			</div>
 			<div className='grid-circle'>
 				<div className='circle-container'>
 					<div className='column1'>
-						<div className='circle'>25</div>
-						<div className='circle'>26</div>
+						<div
+							className={`circle ${selectedDesk === 25 ? 'selected' : ''}`}
+							onClick={() => handleDeskClick(25)}
+						>
+							25
+						</div>
+						<div
+							className={`circle ${selectedDesk === 26 ? 'selected' : ''}`}
+							onClick={() => handleDeskClick(26)}
+						>
+							26
+						</div>
 					</div>
 					<div className='column2'>
-						<div className='circle'>27</div>
-						<div className='circle'>28</div>
+						<div
+							className={`circle ${selectedDesk === 27 ? 'selected' : ''}`}
+							onClick={() => handleDeskClick(27)}
+						>
+							27
+						</div>
+						<div
+							className={`circle ${selectedDesk === 28 ? 'selected' : ''}`}
+							onClick={() => handleDeskClick(28)}
+						>
+							28
+						</div>
 					</div>
 				</div>
 				<div className='circle-container'>
 					<div className='column1'>
-						<div className='circle'>29</div>
-						<div className='circle'>30</div>
+						<div
+							className={`circle ${selectedDesk === 29 ? 'selected' : ''}`}
+							onClick={() => handleDeskClick(29)}
+						>
+							29
+						</div>
+						<div
+							className={`circle ${selectedDesk === 30 ? 'selected' : ''}`}
+							onClick={() => handleDeskClick(30)}
+						>
+							30
+						</div>
 					</div>
 					<div className='column2'>
-						<div className='circle'>31</div>
-						<div className='circle'>32</div>
+						<div
+							className={`circle ${selectedDesk === 31 ? 'selected' : ''}`}
+							onClick={() => handleDeskClick(31)}
+						>
+							31
+						</div>
+						<div
+							className={`circle ${selectedDesk === 32 ? 'selected' : ''}`}
+							onClick={() => handleDeskClick(32)}
+						>
+							32
+						</div>
 					</div>
 				</div>
 			</div>
+			<Button
+				component={Link}
+				to='/home'
+				sx={{
+					backgroundColor: '#3AB54A',
+					color: '#FFFFFF',
+					borderRadius: '20px',
+				}}
+			>
+				Accept
+			</Button>
 		</>
 	);
 }
