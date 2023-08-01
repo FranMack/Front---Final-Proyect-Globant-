5723d0aeca4202c269cfc3c586206e61c1928f61
FranMack <franmack556@gmail.com>
2023-07-31 22:44:14 -0300

    Report home office

--- src/view/HomeList.jsx
+++ src/view/HomeList.jsx
@@ -1,63 +1,46 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import GeocodeUser from '../utils/hookGeocodeUser';
-import React, { useEffect, useState } from 'react';
+import React, {useState } from 'react';
 import axios from 'axios';
 import ResponsiveAppBar from '../components/Navbar';
-import {
-	FormControl,
-	InputLabel,
-	Input,
-	Select,
-	MenuItem,
-	Box,
-	Button,
-	Typography,
-} from '@mui/material';
-import OfficeMap from '../components/OfficeMap';
-import { haversineDistance } from '../utils/calculus';
+import { FormControl, Input, Box, Button } from '@mui/material';
 import useUserLocation from '../utils/hookUserLocation';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
 import { toast } from 'react-toastify';
 
 const HomeList = () => {
-
 	const geocodeUserLocation = GeocodeUser();
-	
+
 	const navigate = useNavigate();
-	const [officeList, setOfficeList] = useState([]);
+
 	const [selectedOffice, setSelectedOffice] = useState('');
 	const [selectedDeskNumber, setSelectedDeskNumber] = useState(null);
-	
-	const [selectedLocation, setSelectedLocation] = useState('');
-	const [adress,setAdress]=useState(null)
-	const [eddit,setEddit]=useState(false)
 
-	const clearAdress=()=>{
-		setAdress("Insert your adress")
-	}
 
-	const handleAdress=(event)=>{
-		setAdress(event.target.value)
-	}
+	const [adress, setAdress] = useState(null);
+	const [eddit, setEddit] = useState(false);
+	const [edditButton, setEditButton] = useState('EDIT');
 
-	const handleEddit=()=>{
-		if(eddit){
-			setEddit(!eddit)
-		}
 
-		else{
 
-			setAdress(null)
-			setEddit(!eddit)
-		}
-		
-	}
-
-	const handleNearbyOfficeChange = event => {
-		setSelectedLocation(event.target.value);
+	const handleAdress = event => {
+		setAdress(event.target.value);
 	};
+
+	const handleEddit = () => {
+		if (eddit) {
+			setEditButton('EDIT');
+			setEddit(!eddit);
+		} else {
+			setAdress(null);
+			setEditButton('GPS ADRESS');
+			setEddit(!eddit);
+		}
+	};
+
+
 
 	const dateReport = new Date()
 		.toLocaleDateString('es-AR')
@@ -68,32 +51,6 @@ const HomeList = () => {
 	const user = useSelector(state => state.user);
 	const report = useSelector(state => state.report);
 	const userLocation = useUserLocation();
-
-	const filterNearbyOffices = radius => {
-		radius = 500;
-		if (!userLocation) return [];
-
-		const nearbyOffices = officeList.filter(office => {
-			const distance = haversineDistance(
-				userLocation.lat,
-				userLocation.lng,
-				office.latitude,
-				office.longitude,
-			);
-
-			return distance <= radius;
-		});
-
-		return nearbyOffices;
-	};
-
-	const handleOfficeChange = event => {
-		const selectedLocation = event.target.value;
-		const selectedOfficeData = officeList.find(
-			office => office._id === selectedLocation,
-		);
-		setSelectedOffice(selectedOfficeData);
-	};
 
 
 
@@ -108,8 +65,8 @@ const HomeList = () => {
 				location: adress || geocodeUserLocation,
 				latitude: userLocation.lat,
 				longitude: userLocation.lng,
-				homeoffice: false,
-				box_number: selectedDeskNumber,
+				homeoffice: true,
+				box_number: 0,
 				status_report: 'Open',
 				date_report: dateReport,
 			};
@@ -131,12 +88,17 @@ const HomeList = () => {
 		}
 	};
 
-
-
-
 	console.log('adress', adress);
 	return (
-		<Box component='form' onSubmit={handleSubmitNewReport} sx={{display:"flex",flexDirection:"column", justifyContent:"center"}}>
+		<Box
+			component='form'
+			onSubmit={handleSubmitNewReport}
+			sx={{
+				display: 'flex',
+				flexDirection: 'column',
+				justifyContent: 'center',
+			}}
+		>
 			<ResponsiveAppBar />
 			<Box
 				style={{
@@ -165,17 +127,20 @@ const HomeList = () => {
 					marginTop: '10px',
 				}}
 			>
-				<Typography variant='body1' sx={{marginBottom:"5%",marginTop:"5%"}}>
+				<h3 style={{ marginBottom: '5%', marginTop: '5%' }}>
 					Please check your adress
-				</Typography>
+				</h3>
 
 				<FormControl style={{ width: '90%' }}>
-					
 					<Input
-					disabled={!eddit}
-					 value={adress===null ? geocodeUserLocation:adress}
-					onChange={handleAdress}/>
-					<Button onClick={handleEddit} sx={{color: '#3AB54A',}}>Edit</Button>
+						sx={{ textAlign: 'center' }}
+						disabled={!eddit}
+						value={adress === null ? geocodeUserLocation : adress}
+						onChange={handleAdress}
+					/>
+					<Button onClick={handleEddit} sx={{ color: '#3AB54A' }}>
+						{edditButton}
+					</Button>
 				</FormControl>
 
 				<>
